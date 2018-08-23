e5dea4327d1807859d97c8fd267261f2cd2e6607
Evan Stern <evanmicahstern@gmail.com>
2018-08-23 12:17:02 -0400

    Add nativestorage plugin and add support for env vars

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -5,126 +5,107 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/js/index.js',
+// eslint-disable-next-line
+module.exports = (env) => {
+  return {
+    entry: './src/js/index.js',
 
-  devtool: 'source-map',
+    devtool: 'source-map',
 
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'www'),
-  },
+    output: {
+      filename: '[name].[hash].js',
+      path: path.resolve(__dirname, 'www'),
+    },
 
-  module: {
-    rules: [
-      // babel loader
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['transform-class-properties'],
-          },
-        },
-      },
-
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: 'css-loader' },
-          {
-            loader: 'postcss-loader',
+    module: {
+      rules: [
+        // babel loader
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              // eslint-disable-next-line
-              plugins: function () {
-                return [
-                  // eslint-disable-next-line
-                  require('precss'),
-                  // eslint-disable-next-line
-                  require('autoprefixer'),
-                ];
-              },
+              presets: ['@babel/preset-env'],
+              plugins: ['transform-class-properties'],
             },
           },
-          { loader: 'sass-loader' },
-        ],
-      },
+        },
 
-      /*
-      // sass loader
-      {
-        test: /\.(scss)$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'postcss-loader',
-            options: {
-              // eslint-disable-next-line
-              plugins: function () {
-                return [
-                  // eslint-disable-next-line
-                  require('precss'),
-                  // eslint-disable-next-line
-                  require('autoprefixer'),
-                ];
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: 'css-loader' },
+            {
+              loader: 'postcss-loader',
+              options: {
+                // eslint-disable-next-line
+                plugins: function () {
+                  return [
+                    // eslint-disable-next-line
+                    require('precss'),
+                    // eslint-disable-next-line
+                    require('autoprefixer'),
+                  ];
+                },
               },
             },
-          },
-          { loader: 'sass-loader' },
-        ],
-      },
-      */
-
-      // html loader
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
-          options: { minimize: true },
+            { loader: 'sass-loader' },
+          ],
         },
-      },
+
+        // html loader
+        {
+          test: /\.html$/,
+          use: {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new webpack.NamedModulesPlugin(),
+
+      // ensure that globally accessed variables in 3rd party libraries are
+      // defined (knockout, pager, bootstrap)
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        ko: 'knockout',
+      }),
+
+      // clean out the www folder between builds to prevent old files from piling
+      // up
+      new CleanWebpackPlugin([
+        'www/',
+      ]),
+
+      new MiniCssExtractPlugin({
+        filename: '[name].[hash].css',
+        chunkFilename: '[id].[hash].css',
+      }),
+
+      // copy all the template files so that the app can access them.
+      new CopyWebpackPlugin([
+        {
+          from: './src/templates/**/*',
+          to: 'templates',
+          flatten: true,
+        },
+      ]),
+
+      // use a template to define the index file
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'src/index.html',
+      }),
+
+      new webpack.DefinePlugin({
+        API_URL: env.NODE_ENV === 'production' ? JSON.stringify('production-url') : JSON.stringify('development-url'),
+      }),
     ],
-  },
-
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-
-    // ensure that globally accessed variables in 3rd party libraries are
-    // defined (knockout, pager, bootstrap)
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      ko: 'knockout',
-    }),
-
-    // clean out the www folder between builds to prevent old files from piling
-    // up
-    new CleanWebpackPlugin([
-      'www/',
-    ]),
-
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
-      chunkFilename: '[id].[hash].css',
-    }),
-
-    // copy all the template files so that the app can access them.
-    new CopyWebpackPlugin([
-      {
-        from: './src/templates/**/*',
-        to: 'templates',
-        flatten: true,
-      },
-    ]),
-
-    // use a template to define the index file
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
-    }),
-  ],
+  };
 };
