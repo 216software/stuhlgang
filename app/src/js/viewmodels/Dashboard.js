ffa09410f6accde2b61c54f9988cdb07def2c4e5
Evan Stern <evanmicahstern@gmail.com>
2018-08-27 14:59:02 -0400

    Draw patient links

--- app/src/js/viewmodels/Dashboard.js
+++ app/src/js/viewmodels/Dashboard.js
@@ -1,9 +1,25 @@
+import ko from 'knockout';
 import BasePage from './BasePage';
 
 class Dashboard extends BasePage {
   constructor () {
     super();
-    this.patients = [];
+    this.patients = ko.observableArray([
+      {
+        id: 1,
+        name: 'Billy Billson',
+      },
+      {
+        id: 2,
+        name: 'Johnny Johnson',
+      },
+    ]);
+
+    this.patientUrl = ({ id }) => ko.computed(() => `/patients/${id}`, this);
+  }
+
+  isEmpty () {
+    return !!(this.patients().length === 0);
   }
 }
 
