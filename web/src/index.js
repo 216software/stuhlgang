5d9e0dd22a7d24a7ddb3cc1dcaf6da503c3c44ba
W. Matthew Wilson <matt@216software.com>
2019-01-21 17:19:37 -0500

    Now got twitter bootstrap stylin OK, at least it seems like it

---
+++ web/src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function () {
+    console.log("Whoomp there it is!");
+});
---
+++ web/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+
+  mode: "development",
+
+  entry: './src/index.js',
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    contentBase: './dist'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+
+    // This plugin makes these available everywhere!
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      ko: 'knockout',
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+
+};
